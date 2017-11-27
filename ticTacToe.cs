122a86bc60deaaea52bf1d1cfad5304d9b04c16c
Justin Johnson <triplejay2013@gmail.com>
2017-11-27 13:16:17 -0700

    Added winning logic

--- ticTacToe.cs
+++ ticTacToe.cs
@@ -7,97 +7,116 @@ using System.Drawing;
 using System.Windows.Forms;
 
 class Control {//Singleton class
-	private static Control instance = null;
-	Player[] players;
-	Player currentPlayer;
+    private static Control instance = null;
+    Player[] players;
+    Player currentPlayer;
 
-	public void Next(){
-		if(currentPlayer.GetNumber()==1){
-			currentPlayer = players[1];
-		} else if(currentPlayer.GetNumber()==2){
-			currentPlayer = players[0];
-		}
-	}
+    public void Next() {
+        if(currentPlayer.GetNumber()==1) {
+            currentPlayer = players[1];
+        } else if(currentPlayer.GetNumber()==2) {
+            currentPlayer = players[0];
+        }
+    }
 
-	public Player Current(){
-		return currentPlayer;
-	}
+    public Player Current() {
+        return currentPlayer;
+    }
 
-	private Control(){
-		Init();
-	}
+    private Control() {
+        Init();
+    }
 
-	public static Control GetInstance(){
-		if(instance==null)
-			instance=new Control();
-		return instance;
-	}
+    public static Control GetInstance() {
+        if(instance==null)
+            instance=new Control();
+        return instance;
+    }
 
-	private void Init(){
-		players = new Player[2];
-		players[0] = new Player(true, 1);//Player one "X"
-		players[1] = new Player(false, 2);//Player two "O"
-		currentPlayer = players[0];
-	}
+    private void Init() {
+        players = new Player[2];
+        players[0] = new Player(true, 1);//Player one "X"
+        players[1] = new Player(false, 2);//Player two "O"
+        currentPlayer = players[0];
+    }
 
-	public bool GetTurn(int playerNumber){
-		//not indexed at 0 to avoid confusion
-		//ex: playerOne == 1 and playerTwo == 2
-		//if(playerNumber > 0 && playerNumber <= 2)
-			return players[playerNumber-1].GetTurn();
-		//else throw exception
-	}
+    public bool GetTurn(int playerNumber) {
+        //not indexed at 0 to avoid confusion
+        //ex: playerOne == 1 and playerTwo == 2
+        //if(playerNumber > 0 && playerNumber <= 2)
+        return players[playerNumber-1].GetTurn();
+        //else throw exception
+    }
 
-	public string GetMark(int playerNumber){
-		//not indexed at 0 to avoid confusion
-		//ex: playerOne == 1 and playerTwo == 2
-		//if(playerNumber > 0 && playerNumber <= 2)
-		return players[playerNumber-1].GetMark();
-		//else throw exception
-	}
+    public string GetMark(int playerNumber) {
+        //not indexed at 0 to avoid confusion
+        //ex: playerOne == 1 and playerTwo == 2
+        //if(playerNumber > 0 && playerNumber <= 2)
+        return players[playerNumber-1].GetMark();
+        //else throw exception
+    }
 }
 
 class Player {
-	bool playersTurn;
-	int playerNumber;
-	string playerSymbol;
+    bool playersTurn;
+    int playerNumber;
+    string playerSymbol;
+	bool playerWins=false;
 
-	public int GetNumber(){
-		return playerNumber;
-	}
-
-	public bool GetTurn(){
-		return playersTurn;
+	public bool GetWin(){
+		return playerWins;
 	}
 
-	public string GetMark(){
-		return playerSymbol;
+	public void SetWin(bool flag){
+		playerWins=flag;
 	}
 
-	public Player(int num){
-		playerNumber=num;
-		playersTurn=false;
-		if(playerNumber==1){playerSymbol="X";}
-		else if(playerNumber==2){playerSymbol="O";}
-	}
-	
-	public Player(bool turn){
-		playerNumber=1;
-		playersTurn=turn;
-	}
-	
-	public Player(bool turn, int num){
-		playerNumber=num;
-		playersTurn=turn;
-		if(playerNumber==1){playerSymbol="X";}
-		else if(playerNumber==2){playerSymbol="O";}
-	}
+    public int GetNumber() {
+        return playerNumber;
+    }
+
+    public bool GetTurn() {
+        return playersTurn;
+    }
+
+    public string GetMark() {
+        return playerSymbol;
+    }
+
+    public Player(int num) {
+        playerNumber=num;
+        playersTurn=false;
+        if(playerNumber==1) {
+            playerSymbol="X";
+        }
+        else if(playerNumber==2) {
+            playerSymbol="O";
+        }
+    }
+
+    public Player(bool turn) {
+        playerNumber=1;
+        playersTurn=turn;
+    }
+
+    public Player(bool turn, int num) {
+        playerNumber=num;
+        playersTurn=turn;
+        if(playerNumber==1) {
+            playerSymbol="X";
+        }
+        else if(playerNumber==2) {
+            playerSymbol="O";
+        }
+    }
 }
 
 class MForm : Form {
-	Control control = Control.GetInstance();
+    Control control = Control.GetInstance();
+    bool gameOver = false;
+    Button[,] grid = new Button[3,3];
 
-	private void init(){
+    private void init() {
         Text = "Tic Tac Toe";
         Size = new Size(325, 350);
         String btn1String = "";
@@ -118,7 +137,8 @@ class MForm : Form {
         btn1.Parent = this;
         btn1.Location = new Point(0, 0);
         btn1.Size = new Size(100, 100);
-		btn1.Click += new EventHandler(this.selectTile);
+        btn1.Click += new EventHandler(this.selectTile);
+        grid[0,0] = btn1;
 
         Button btn2 = new Button();
         btn2.Text = btn2String;
@@ -126,7 +146,8 @@ class MForm : Form {
         btn2.Parent = this;
         btn2.Location = new Point(100, 0);
         btn2.Size = new Size(100, 100);
-		btn2.Click += new EventHandler(this.selectTile);
+        btn2.Click += new EventHandler(this.selectTile);
+        grid[0,1] = btn2;
 
         Button btn3 = new Button();
         btn3.Text = btn3String;
@@ -134,7 +155,8 @@ class MForm : Form {
         btn3.Parent = this;
         btn3.Location = new Point(200, 0);
         btn3.Size = new Size(100, 100);
-		btn3.Click += new EventHandler(this.selectTile);
+        btn3.Click += new EventHandler(this.selectTile);
+        grid[0,2] = btn3;
 
         Button btn4 = new Button();
         btn4.Text = btn4String;
@@ -142,23 +164,26 @@ class MForm : Form {
         btn4.Parent = this;
         btn4.Location = new Point(0, 100);
         btn4.Size = new Size(100, 100);
-		btn4.Click += new EventHandler(this.selectTile);
-         
+        btn4.Click += new EventHandler(this.selectTile);
+        grid[1,0] = btn4;
+
         Button btn5 = new Button();
         btn5.Text = btn5String;
         btn5.Font = new Font(btn5.Font.FontFamily, fontSize);
         btn5.Parent = this;
         btn5.Location = new Point(100, 100);
         btn5.Size = new Size(100, 100);
-		btn5.Click += new EventHandler(this.selectTile);
-        
+        btn5.Click += new EventHandler(this.selectTile);
+        grid[1,1] = btn5;
+
         Button btn6 = new Button();
         btn6.Text = btn6String;
         btn6.Font = new Font(btn6.Font.FontFamily, fontSize);
         btn6.Parent = this;
         btn6.Location = new Point(200, 100);
         btn6.Size = new Size(100, 100);
-		btn6.Click += new EventHandler(this.selectTile);
+        btn6.Click += new EventHandler(this.selectTile);
+        grid[1,2] = btn6;
 
         Button btn7 = new Button();
         btn7.Text = btn7String;
@@ -166,37 +191,149 @@ class MForm : Form {
         btn7.Parent = this;
         btn7.Location = new Point(0, 200);
         btn7.Size = new Size(100, 100);
-		btn7.Click += new EventHandler(this.selectTile);
-        
+        btn7.Click += new EventHandler(this.selectTile);
+        grid[2,0] = btn7;
+
         Button btn8 = new Button();
         btn8.Text = btn8String;
         btn8.Font = new Font(btn8.Font.FontFamily, fontSize);
         btn8.Parent = this;
         btn8.Location = new Point(100, 200);
         btn8.Size = new Size(100, 100);
-		btn8.Click += new EventHandler(this.selectTile);
-        
+        btn8.Click += new EventHandler(this.selectTile);
+        grid[2,1] = btn8;
+
         Button btn9 = new Button();
         btn9.Text = btn9String;
         btn9.Font = new Font(btn9.Font.FontFamily, fontSize);
         btn9.Parent = this;
         btn9.Location = new Point(200, 200);
         btn9.Size = new Size(100, 100);
-		btn9.Click += new EventHandler(this.selectTile);
-	}
+        btn9.Click += new EventHandler(this.selectTile);
+        grid[2,2] = btn9;
+    }
 
     public MForm() {
-		init();
+        init();
 
         CenterToScreen();
     }
 
-	void selectTile(Object sender, EventArgs e){
-		Button clickedButton = (Button) sender;
-		clickedButton.Text = control.GetMark(control.Current().GetNumber()); //use method to select X or O Dependant upon players turn
-		clickedButton.Enabled = false;
-		control.Next();
+    void selectTile(Object sender, EventArgs e) {
+        if(!gameOver) {
+            Button clickedButton = (Button) sender;
+            clickedButton.Text = control.GetMark(control.Current().GetNumber()); //use method to select X or O Dependant upon players turn
+            clickedButton.Enabled = false;
+            checkWin();
+        }
+        if(!gameOver)
+            control.Next();
+    }
+
+    private void checkWin() {
+        int counter = 0;
+        //check for draw
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                if(grid[i,j].Text != "") {
+                    ++counter;
+                }
+            }
+        }
+        if(counter>=9){
+			gameOver = true;
+			GameOverDisplay();
+		}
+		control.Current().SetWin(false);
+
+		//check rows for win
+        for(int i = 0; i < 3; ++i) {
+			counter = 1;
+            for(int j = 0; j < 2; ++j) {
+                if(grid[i,j].Text == grid[i,j+1].Text && grid[i,j].Text != "") {
+					++counter;
+				}
+			}
+			if(counter == 3) break;
+		}
+		if(counter == 3){
+			control.Current().SetWin(true);
+			gameOver = true;
+			GameOverDisplay();
+		}
+
+		//check verticles for win
+		counter = 0;
+		if(grid[0,0].Text == grid[1,0].Text && grid[1,0].Text == grid[2,0].Text && grid[0,0].Text != "") {++counter;}
+		else if(grid[0,1].Text == grid[1,1].Text && grid[1,1].Text == grid[2,1].Text && grid[0,1].Text != "") {++counter;}
+		else if(grid[0,2].Text == grid[1,2].Text && grid[1,2].Text == grid[2,2].Text && grid[0,2].Text != "") {++counter;}
+		if(counter != 0){
+			control.Current().SetWin(true);
+			gameOver = true;
+			GameOverDisplay();
+		}
+
+		//check right diagonal \
+		counter = 0;
+		string match = "";
+		for(int i = 0; i < 3; ++i){
+			if(grid[i, i].Text != "" && i == 0){
+				match = grid[i, i].Text;
+				++counter;
+			}
+			else if(match == grid[i, i].Text && grid[i, i].Text != ""){
+				++counter;
+			}
+		}
+		if(counter == 3){
+			control.Current().SetWin(true);
+			gameOver = true;
+			GameOverDisplay();
+		}
+
+		//check left diagonal /
+		counter = 0;
+		match = "";
+		for(int i = 0; i < 3; ++i){
+			if(grid[i, 2-i].Text != "" && i == 0){
+				match = grid[i, 2-i].Text;
+				++counter;
+			}
+			else if(match == grid[i, 2-i].Text && grid[i, 2-i].Text != ""){
+				++counter;
+			}
+		}
+		if(counter == 3){
+			control.Current().SetWin(true);
+			gameOver = true;
+			GameOverDisplay();
+		}
 	}
+
+    public void GameOverDisplay() {
+        //displays an Alert that notify's user of winner and game over
+		string message;
+		if(control.Current().GetWin()){
+			message = "Player " + control.Current().GetNumber() + " won!";
+		}
+		else{
+			message = "Game ends in a draw!";
+		}
+        string caption = "GAME OVER";
+        MessageBoxButtons buttons = MessageBoxButtons.OK;
+        DialogResult result;
+
+        // Displays the MessageBox.
+        result = MessageBox.Show(message, caption, buttons);
+
+        if (result == System.Windows.Forms.DialogResult.OK)
+        {
+            // Closes the parent form.
+			//TODO: if time, we can make an option to reset the game, or close
+            this.Close();
+
+        }
+    }
 }
 
 
